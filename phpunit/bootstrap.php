651be1880da2c9db47638d61f04247721657f02d
server <server@example.com>
2018-03-08 19:56:20 -0700

    psr4

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -10,4 +10,4 @@ use anytizer\includer;
 /**
  * Now, the includer will manage all inclusions
  */
-spl_autoload_register(array(new includer("../src/libraries/classes"), "namespaced_inc_dot"));
+#spl_autoload_register(array(new includer("../src/libraries/classes"), "namespaced_inc_dot"));
