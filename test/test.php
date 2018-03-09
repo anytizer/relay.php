fe960c7cd1874e53890108cf7ad75215a698fbbe
server <server@example.com>
2018-03-08 20:03:13 -0700

    no includer

--- test/test.php
+++ test/test.php
@@ -3,13 +3,13 @@
  * Copy this file where your vendor/ is.
  */
 require_once "vendor/autoload.php";
-use anytizer\includer;
+#use anytizer\includer;
 
 /**
  * Feature from includer.php to locate relay class.
  */
-$path = dirname(__FILE__)."/src/libraries/classes";
-spl_autoload_register(array(new includer($path), "namespaced_inc_dot"));
+#$path = dirname(__FILE__)."/src/libraries/classes";
+#spl_autoload_register(array(new includer($path), "namespaced_inc_dot"));
 use anytizer\connections\relay;
 
 $_GET = array(
